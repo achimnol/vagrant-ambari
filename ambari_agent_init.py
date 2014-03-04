17548d85d58440c1e6068b8d2edeae4c6d3aa8f8
Joongi Kim <joongi@an.kaist.ac.kr>
2014-03-04 19:11:25 +0900

    Fix a minor mistake

--- ambari_agent_init.py
+++ ambari_agent_init.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 import ConfigParser
 
-if __name__ == '__main__':
-    config = ConfigParser.RawConfigParser()
-    config.read('/etc/ambari-agent/conf/ambari-agent.ini')
-    if not config.has_option('agent', 'hostname_script'):
-        config.set('agent', 'hostname_script', 'hostname')
-        with open('test.ini', 'wb') as f:
-            config.write(f)
+ambari_conf = '/etc/ambari-agent/conf/ambari-agent.ini'
+config = ConfigParser.RawConfigParser()
+config.read(ambari_conf)
+if not config.has_option('agent', 'hostname_script'):
+    config.set('agent', 'hostname_script', 'hostname')
+    with open(ambari_conf, 'wb') as f:
+        config.write(f)
